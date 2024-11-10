c77d473478ec57f5249789782cc04d3c54c2787e
Whoistolu <digittolu25@gmail.com>
2024-11-10 10:28:45 +0100

    Add Edit, Update and Destroy actions

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,8 @@
 class PatientsController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :check_receptionist
+
   def index
     @patients = Patient.all
   end
@@ -17,7 +20,30 @@ class PatientsController < ApplicationController
     end
   end
 
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+    if @patient.update(patient_params)
+      redirect_to patients_path, notice: "Patient updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @patient = Patient.find(params[:id])
+    @patient.destroy
+    redirect_to patients_path, notice: "Patient removed."
+  end
+
   private
+
+  def check_receptionist
+    redirect_to root_path, alert: "Access denied!" unless current_user.receptionist?
+  end
 
   def patient_params
     params.require(:patient).permit(:name, :age, :gender, :phone, :address, :medical_history)
