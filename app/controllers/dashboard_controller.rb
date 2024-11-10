ec037cc9262c1ac0dce6b3af11f308fcf0a2d6eb
Whoistolu <digittolu25@gmail.com>
2024-11-10 10:31:25 +0100

    Allow only doctor users to view the dashboard and see the patient registration chart

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,15 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_doctor
+
+  def index
+    @patients = Patient.all
+    @patients_by_day = Patient.group_by_day(:created_at).count
+  end
+
+  private
+
+  def check_doctor
+    redirect_to root_path, alert: "Access denied!" unless current_user.doctor?
+  end
 end
