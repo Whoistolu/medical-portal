ec889d0eba2d709cdc75d92397082618f3d4f7f8
Whoistolu <digittolu25@gmail.com>
2024-11-10 10:24:31 +0100

    Add the New, Create action  and the Patient-Params to create new patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,4 +3,23 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.all
   end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      redirect_to patients_path, notice: "Patient registered successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:name, :age, :gender, :phone, :address, :medical_history)
+  end
 end
