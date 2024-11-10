e612db314bc231adbe052c608a59cb3663ba8f78
Whoistolu <digittolu25@gmail.com>
2024-11-10 10:00:29 +0100

    Route for doctor's dashboard to view patients and charts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
 
   resources :patients, only: [:index, :new, :create, :edit, :update, :destroy]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'dashboard', to: 'dashboard#index'
 end
