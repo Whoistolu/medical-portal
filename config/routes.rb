6e6ba6019af281205617dc2e1d8ee42b01d0db8c
Whoistolu <digittolu25@gmail.com>
2024-11-11 14:24:10 +0100

    Set the welcome/splash screen as my root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'dashboard#index'
+  root 'welcome#index'  #Set the splash screen as my root
 
   devise_for :users
-
+  
+  # Patients routes
   resources :patients, only: [:index, :new, :create, :edit, :update, :destroy]
 
+  # Dashboard route
   get 'dashboard', to: 'dashboard#index'
 end
