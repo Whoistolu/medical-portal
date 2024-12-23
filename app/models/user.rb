22a2a37e8dbd8461a339802293d5eca4cada2136
Whoistolu <digittolu25@gmail.com>
2024-12-23 11:00:43 +0100

    Create doctor

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   enum role: { receptionist: 0, doctor: 1 }
 
