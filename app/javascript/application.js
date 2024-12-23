e1484b2ad311f04f033afafa36f9b3c6618503a6
Whoistolu <digittolu25@gmail.com>
2024-12-23 11:01:14 +0100

    Update application.js file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import * as bootstrap from "bootstrap"
+import "@hotwired/turbo-rails";
+import "controllers";
+import * as bootstrap from "bootstrap";
+import "chartkick";
+import "Chart.bundle";
+
+import Rails from "@rails/ujs"; // Import UJS library explicitly
+Rails.start(); // Start UJS to enable method links
